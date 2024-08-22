179fc37e34ea9171ee4ec266b4ef177aca4cb56a
Justicesibisi <djmangsa@gmail.com>
2024-08-22 12:46:42 +0200

    Classes

--- Cat.java
+++ Cat.java
@@ -1,24 +1,26 @@
-public class Main {
+public class Cat {
+    //Attributes
     String type;
     int age;
     String behavior;
     double averageweight;
     String Commoncolour;
-
-    public Main(String type, int age, String behavior, double averageweight, String Commoncolour) {
+    //Constructor
+    public Cat(String type, int age, String behavior, double averageweight, String Commoncolour) //Parameters//
+    {
         this.type = type;
         this.age = age;
         this.behavior = behavior;
         this.averageweight = averageweight;
         this.Commoncolour = Commoncolour;
     }
-
-  /* */  public void displayInfo() {
-     //   System.out.println("Type : " + type);
-      //  System.out.println("Age : " + age);
-       // System.out.println("Behavior : " + behavior);
-       // System.out.println("AverageWeight : " + averageweight + " kg");
-      //  System.out.println("CommonColour : " + Commoncolour);
+    //Method
+    public void displayInfo() {
+        System.out.println("Type : " + type);
+        System.out.println("Age : " + age);
+        System.out.println("Behavior : " + behavior);
+        System.out.println("AverageWeight : " + averageweight + " kg");
+        System.out.println("CommonColour : " + Commoncolour);
     
         
     }
--- CatTest.java
+++ CatTest.java
@@ -1,7 +1,7 @@
 public class CatTest {
     public static void main(String[] args) {
-        Main Cat1 = new Main("Domestic cat", 5, "Human friendly", 4.5, "Black stripes with brown background");
-        Main Cat2 = new Main("Mountain cat", 1, "Unpredictable, dangerous to human", 8.2, "pale gray in winter and darker brown in summer");
+        Cat Cat1 = new Cat("Domestic cat", 5, "Human friendly", 4.5, "Black stripes with brown background");
+        Cat Cat2 = new Cat("Mountain cat", 1, "Unpredictable, dangerous to human", 8.2, "pale gray in winter and darker brown in summer");
 
         Cat1.displayInfo();
         System.out.println();
